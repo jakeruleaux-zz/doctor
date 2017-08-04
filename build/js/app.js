c55ca3bf9ee7c900ea32dda3e29c3a08f1991776
jacob ruleaux <jakeruleaux@gmail.com>
2017-08-04 15:25:00 -0700

    clean up commented code

--- build/js/app.js
+++ build/js/app.js
@@ -3,44 +3,25 @@ exports.apiKey = "08b0839620e548c802aa07e96f29fb1c";
 
 },{}],2:[function(require,module,exports){
 var apiKey = require('./../.env').apiKey;
-console.log(apiKey);
+
 function Doctor(medicalIssue) {
-  // this.doctorsName = doctorsName;
   this.medicalIssue = medicalIssue;
-  // console.log(medicalIssue);
+
 }
 
 
-Doctor.prototype.getDoctors = function(medicalIssue) {
+Doctor.prototype.getDoctors = function(medicalIssue, listDoctors) {
   var medicalIssue = this.medicalIssue;
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(response) {
      var medics = response.data;
-    //  listDoctors(medics);
+     listDoctors(medicalIssue, medics);
      console.log(response);
     })
    .fail(function(error){
       console.log("fail");
     });
 };
-
-// Doctor.prototype.doctorsNames = function() {
-//   var allDoctors = [];
-//   console.log(allDoctors);
-//   var medicalIssue = this.medicalIssue;
-//     response.then(function(response) {
-//     for (i = 0; i < medicalIssue; i++) {
-//       var listofdoctors = response[i].join(" ");
-//       listDoctors(listofdoctors);
-//     }
-//     })
-//     .fail(function(error) {
-//       $("#output").text(error.responseJSON.message);
-//     });
-// };
-
-
-
 
 exports.doctorModule = Doctor;
 
